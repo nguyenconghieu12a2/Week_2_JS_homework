c023da239b5b37c0543838bf424b0379e9c70a92
nguyenconghieu12a2 <141993615+nguyenconghieu12a2@users.noreply.github.com>
2024-02-24 14:24:48 +0700

    Add files via upload

---
+++ Week_2/homework.js
@@ -0,0 +1,24 @@
+function upDate(element){
+    //    In this function you should 
+    //    1) change the url for the background image of the div with the id = "image" 
+    //    to the source file of the preview image
+    var previewPicSrc = element.src;
+    var previewPicAlt = element.alt;
+    var galleryImage = document.getElementById('image');
+    //    2) Change the text  of the div with the id = "image" 
+    //    to the alt text of the preview image 
+    galleryImage.innerHTML = previewPicAlt;
+    galleryImage.style.backgroundImage = "url('" + previewPicSrc + "')";
+     
+       }
+   
+       function unDo(){
+    //    In this function you should 
+    //    1) Update the url for the background image of the div with the id = "image" 
+    //    back to the orginal-image.  You can use the css code to see what that original URL was
+    var galleryImage = document.getElementById('image');
+    //    2) Change the text  of the div with the id = "image" 
+    //    back to the original text.  You can use the html code to see what that original text was
+    galleryImage.innerHTML = "Hover over an image below to display here.";
+    galleryImage.style.backgroundImage = "none";
+       }
